445d93e4165224c8958c4cd3b32d462f382c0265
tarusovadarina6 <tarusovadarina6@gmail.com>
2025-08-19 01:38:17 +0300

    Переделано позиционирование кружочков

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,22 @@ import { Circle } from "./Circle";
 function App() {
   return (
     <main className="container-2xl mx-auto">
-      <section className="z-[var(--z-base)] py-[80px] px-[30px] bg-dark-grey w-screen h-fit grid grid-cols-[repeat(auto-fit,minmax(300px,var(--spacing-card-max-width)))] justify-center items-center gap-mini-gap rounded-section lg:self-center">
-        <Circle className={"top-[-5%] left-[40%] z-[var(--z-circle-1)]"} />
-        <Circle className={"top-px-0 left-[45%] z-[var(--z-circle-2)]"} />
-        <Circle className={"bottom-[-5%] left-[43%] z-[var(--z-circle-3)]"} />
+      <section className="z-[var(--z-base)] py-[80px] px-[30px] bg-dark-grey w-screen h-fit grid grid-cols-[repeat(auto-fit,minmax(300px,var(--spacing-card-max-width)))] justify-center items-center gap-mini-gap rounded-section lg:self-center relative overflow-hidden">
+        <Circle
+          className={
+            "z-[var(--z-circle-1)] left-5 -top-28  md:top-14 md:right-24 xl:bottom-[234px] xl:left-[632px]"
+          }
+        />
+        <Circle
+          className={
+            "z-[var(--z-circle-3)] top-2 left-20md:bottom-5 md:right-3.5 xl:bottom-[181px] xl:right-[404px]"
+          }
+        />
+        <Circle
+          className={
+            "z-[var(--z-circle-2)]  -top-24 left-24  md:bottom-20 md:right-50 xl:top-[217px] xl:left-[746px]"
+          }
+        />
         {data.map((item, index) => (
           <Card
             key={index}
--- src/Circle.jsx
+++ src/Circle.jsx
@@ -2,7 +2,7 @@ import "./index.css";
 export const Circle = ({ className }) => {
   return (
     <div
-      className={`${className} rounded-full w-circle-size h-circle-size bg-[linear-gradient(141.33deg,var(--color-dark-grey)50.14%,var(--color-dark-medium-grey)108.81%)] absolute`}
+      className={`${className} rounded-full w-circle-mini-size h-circle-mini-size bg-[linear-gradient(141.33deg,var(--color-dark-grey)50.14%,var(--color-dark-medium-grey)108.81%)] absolute md:w-circle-size md:h-circle-size`}
     ></div>
   );
 };
